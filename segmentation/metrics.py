3ddba2cc41612a3ba498afdb8ac042c22f167507
dranaivo <dranaivo94@gmail.com>
2022-04-25 13:11:04 +0200

    Module for metric computation

---
+++ segmentation/metrics.py
@@ -0,0 +1,9 @@
+class AccuracyMetric():
+    def __init__(self):
+        pass
+    def reset(self):
+        pass
+    def update_values(self):
+        pass
+    def get_values(self):
+        pass
