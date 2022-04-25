77578b02216491ee8730f034a0a4560e33190510
dranaivo <dranaivo94@gmail.com>
2022-04-25 15:01:40 +0200

    Method for loading checkpoint in Engine

--- segmentation/engine.py
+++ segmentation/engine.py
@@ -1,6 +1,26 @@
-# train_step
+# import libraries
+import shutil
 
-total_iter += 1
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import tqdm
+
+from data import DatasetCityscapes
+from metrics import AccuracyMetric
+import numpy as np
+
+from PIL import Image
+import random
+
+import glob
+from os.path import join
+
+from PIL import ImageFile
+
+import torchvision.transforms as transforms
+
+ImageFile.LOAD_TRUNCATED_IMAGES = True
 
 # def training_step(data_iter, network, loss_fn, optimizer, args):
 #     input, target = data_iter.next()
@@ -16,7 +36,7 @@ total_iter += 1
 #     optimizer.step()
 
 class Engine():
-    def __ini__(self, args, network, loss_fn, optimizer):
+    def __init__(self, args, network, loss_fn, optimizer):
         self.args = args
         self.network = network
         self.loss_fn = loss_fn
@@ -41,16 +61,20 @@ class Engine():
             self.description = "[Testing]"
 
         self.set_dataloader = DatasetCityscapes(opt=args, phase=phase, data_transform=data_transform)
-        self.dataloader = torch.utils.data.DataLoader(set_dataloader, batch_size=args.batch_size, shuffle=shuffle,
+        self.dataloader = torch.utils.data.DataLoader(self.set_dataloader, batch_size=args.batch_size, shuffle=shuffle,
                                                            num_workers=args.n_threads, drop_last=True)
         print("Dataset size : ", len(self.dataloader))
         self.progress_bar = tqdm.tqdm(range(len(self.dataloader)))
-        self.total_iter = 0
+        self.total_epochs = args.total_epochs
         self.global_cm = 0
-        self.accuracy_metric = AccuracyMetric(global_cm=global_cm)
+        self.accuracy_metric = AccuracyMetric(global_cm=self.global_cm)
 
     def load_checkpoint(self):
-        pass
+        checkpoint = torch.load(glob.glob(join(self.args.path_to_checkpoints, "*latest*.pt"))[0])
+        self.network.load_state_dict(checkpoint['model_state_dict'])
+        self.optimizer.load_state_dict(checkpoint['optim_state_dict'])
+        # epoch = checkpoint['epoch']
+
     def train(self):
         pass
     def evaluate(self):
