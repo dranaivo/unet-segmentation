a141d46f215cad8eb34b00bc7045a76b34db1387
dranaivo <dranaivo94@gmail.com>
2022-04-25 13:16:35 +0200

    Metrics definition and getter

--- segmentation/metrics.py
+++ segmentation/metrics.py
@@ -1,9 +1,13 @@
 class AccuracyMetric():
-    def __init__(self):
-        pass
+    def __init__(self, global_cm):
+        self.global_cm = global_cm
+        self.overall_acc = 0
+        self.average_acc = 0
+        self.average_iou = 0
+
     def reset(self):
         pass
     def update_values(self):
         pass
     def get_values(self):
-        pass
+        return self.overall_acc, self.average_acc, self.average_iou
