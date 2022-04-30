16aeebebb984301ac13966fb8382b7e56c3dd0cb
dranaivo <dranaivo94@gmail.com>
2022-04-30 12:44:52 +0200

    Tmp script for cuda

--- scripts/evaluate.sh
+++ scripts/evaluate.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 python ../segmentation/main.py \
     --test \
+    --batch_size 1 \
+    --cuda \
     --path_to_dataset ../unicityscape \
     --path_to_checkpoints ../snapshots
--- scripts/train.sh
+++ scripts/train.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 python ../segmentation/main.py \
     --train \
-    --total_epochs 4 \
+    --total_epochs 8 \
+    --cuda \
+    --batch_size 1 \
+    --resume \
+    --save \
+    --save_epoch 2 \
     --path_to_dataset ../unicityscape \
     --path_to_checkpoints ../snapshots
